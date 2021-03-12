510aac6a0663441c4843cef9e2a874dccfcc6838
yuan qian <yuanqian@yuandeMacBook-Pro.local>
2021-03-12 16:59:17 +1300

    first submit

---
+++ promise.js
@@ -0,0 +1,23 @@
+(function(window){
+function Promise(excutor){
+  function resolve(value){
+
+  };
+  function reject(reason){
+
+  }
+}
+Promise.prototype.then = function (onResolved,onRejected){
+
+}
+Promise.prototype.catch = function(onResolved,onRejected){
+
+}
+Promise.all = function(parames){
+
+}
+Promise.race = function(parames){
+  
+}
+window.Promise = Promise;
+})(window)
